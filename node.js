bded44032aa837d8be03698ac9bbfd965525a74e
Will M <wmcna97@gmail.com>
2019-04-25 13:28:27 -0400

    added bid function

--- node.js
+++ node.js
@@ -56,12 +56,70 @@ function post(){
             }
         }
     ]).then(function(res){
-        connection.query("INSERT INTO items SET ?"),
+        connection.query("INSERT INTO items SET ?",
         {
             name: res.name,
             high_bid: res.startingBid
-        }
+        })
         console.log("New item added!");
+    })
+}
+
+function bid(){
+    connection.query("SELECT * FROM items", function(err, results) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they'd like to bid on
+        inquirer
+          .prompt([
+            {
+              name: "choice",
+              type: "rawlist",
+              choices: function() {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                  choiceArray.push(results[i].name);
+                }
+                return choiceArray;
+              },
+              message: "What auction would you like to place a bid in?"
+            },
+            {
+              name: "bid",
+              type: "input",
+              message: "How much would you like to bid?",
+              validate: function(value){
+                if (isNaN(value) === false){
+                    return true;
+                }
+                return false;
+            }
+            }
+          ])
+          .then(function(answer) {
+            connection.query("SELECT high_bid FROM items WHERE name = '" + answer.choice + "'", function(err, results){
+                if (err) console.log(err);
+                console.log(results[0].high_bid);
+                console.log(parseInt(answer.bid));
+                if (results[0].high_bid < parseInt(answer.bid)){
+                    connection.query("UPDATE items SET ? WHERE ?",
+                    [
+                        {
+                            high_bid: parseInt(answer.bid)
+                        },
+                        {
+                            name: answer.choice
+                        }
+                    ],
+                    function(err, res) {
+                        console.log("New high bid set!");
+                    })
+                }
+                else {
+                    console.log("Bid not high enough!");
+                    bid();
+                }
+                })
+            })
     })
 }
 
