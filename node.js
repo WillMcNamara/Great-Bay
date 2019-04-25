0d028ef952c3f05cf2cc0350c8063796d53ccbe5
Will M <wmcna97@gmail.com>
2019-04-25 12:33:31 -0400

    added post prompt, not working with sql

--- node.js
+++ node.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -21,7 +22,47 @@ connection.connect(function(err){
 })
 
 function afterConnection(){
-    connection.query("")
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to POST or BID?",
+            choices: ["POST", "BID"],
+            name: "postBid"
+        }
+    ]).then(function(res){
+        if (res.postBid === "POST"){
+            post();
+        }
+        else{
+            bid();
+        }
+    })
+}
+
+function post(){
+    inquirer.prompt([
+        {
+            message: "Name of item?",
+            name: "name"
+        },
+        {
+            message: "Starting bid?",
+            name: "startingBid",
+            validate: function(value){
+                if (isNaN(value) === false){
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function(res){
+        connection.query("INSERT INTO items SET ?"),
+        {
+            name: res.name,
+            high_bid: res.startingBid
+        }
+        console.log("New item added!");
+    })
 }
 
 //connects
@@ -33,3 +74,5 @@ function afterConnection(){
 //if bid, pick item from list, then ask for bid
     //if > highbid, update highbid, consolelog 'new highbid'
     //else consolelog 'bid not high enough'
+
+    
